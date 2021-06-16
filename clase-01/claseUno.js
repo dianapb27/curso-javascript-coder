0c5e3d68266330d603938121016153db413d875c
Diana Pinon <dianapinonb@gmail.com>
2021-06-16 17:39:22 -0500

    Diferencia agregada

--- clase-01/claseUno.js
+++ clase-01/claseUno.js
@@ -1,7 +1,10 @@
 const primerNumero = parseInt(prompt("Ingresa el primer número"));
 const segundoNumero = parseInt(prompt("Ingresa el segundo número"));
 const suma = primerNumero + segundoNumero;
+const diferencia = primerNumero - segundoNumero;
 
 alert("El resultado de tu suma es " + suma);
 
 console.log("Te advertimos que era boba...");
+console.log("Pero ya le estamos enseñando a restar..."); 
+console.log("La diferencia entre el primer y segundo número es " + diferencia);
