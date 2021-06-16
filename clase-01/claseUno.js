ac0f99a410afd8be26e5833976130d25a9254443
Diana Pinon <dianapinonb@gmail.com>
2021-06-15 20:59:28 -0500

    Entregable clase uno finalizado

---
+++ clase-01/claseUno.js
@@ -0,0 +1,7 @@
+const primerNumero = parseInt(prompt("Ingresa el primer número"));
+const segundoNumero = parseInt(prompt("Ingresa el segundo número"));
+const suma = primerNumero + segundoNumero;
+
+alert("El resultado de tu suma es " + suma);
+
+console.log("Te advertimos que era boba...");
