7ae64b2cbf22b745df0fad95b4a0911db79427f2
Diana Pinon <dianapinonb@gmail.com>
2021-06-25 22:21:51 -0500

    Desafio complementario clase 4 creado

---
+++ complementario-clase-04/compClaseCuatro.js
@@ -0,0 +1,13 @@
+const precioOriginal = Number(prompt("Ingresa el precio original"));
+const descuento = Number(prompt("Ingresa el descuento (entre 1 y 100)"));
+
+function nuevoPrecio(precioOriginal, descuento) {
+  return precioOriginal * (1 - (descuento / 100));
+}
+
+function descuentoTotal(precioOriginal, descuento) {
+  return precioOriginal * (descuento / 100);
+}
+
+alert("El precio despues del descuento es $" + nuevoPrecio(precioOriginal, descuento));
+console.log("El total de la rebaja es $" + descuentoTotal(precioOriginal, descuento));
