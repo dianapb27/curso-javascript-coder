7b3b78d4a531bca1efe56d34733b78ff32631415
Diana Pinon <dianapinonb@gmail.com>
2021-07-01 20:37:14 -0500

    Creada practica clase 6

---
+++ practica-clase-6/claseSeis.js
@@ -0,0 +1,8 @@
+const productos = [{ id: 1, producto: "Arroz"},
+                  { id: 2, producto: "Fideo"},
+                  { id: 3, producto: "Pan"}];
+
+for (const producto of productos) {
+  console.log(producto.id);
+  console.log(producto.producto);
+}
