bfeaff908a837cfc224b52df3363733edec01bcf
Diana Pinon <dianapinonb@gmail.com>
2021-07-20 19:25:31 -0500

    Creado folder para practica clase 8 - DOM

---
+++ practica-clase-8/main.js
@@ -0,0 +1,18 @@
+// Elegir todo el documento
+document;
+
+// Elegir head & body
+document.body;
+document.head;
+
+// Tomar elemento por id - solo 1 objeto
+document.getElementById("parrafo1");
+document.querySelector("#parrafo1");
+
+// Tomar elementos por classes - coleccion de objetos
+document.getElementsByClassName("texto");
+document.querySelectorAll(".texto");
+
+// Tomar elementos por etiqueta - coleccion de objetos
+document.getElementsByTagName("p");
+document.querySelectorAll("p");
