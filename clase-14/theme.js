c8ce412ce6bd0c83b25d7dc852902b5556eb6f00
Diana Pinon <dianapinonb@gmail.com>
2021-08-17 15:18:42 -0500

    Creado folder clase 14

---
+++ clase-14/theme.js
@@ -0,0 +1,41 @@
+function darkMode() {
+  $("#theme-btn").empty()
+             .append('<i class="fas fa-sun"></i>');
+  $("body").css("background-color", "black");
+  $("h1").css("color", "aquamarine");
+  $("h4").addClass("text-white");
+  $("p").css("color", "white");
+  $(".card").addClass("bg-dark");
+  $(".card-text").addClass("text-light");
+  $("input").addClass("bg-dark text-light");
+  localStorage.setItem("theme", "dark");
+}
+
+function lightMode() {
+  $("#theme-btn").empty()
+             .append('<i class="fas fa-moon"></i>')
+  $("body").css("background-color", "white");
+  $("h1").css("color", "teal");
+  $("h4").removeClass("text-white");
+  $("p").css("color", "black");
+  $(".card").removeClass("bg-dark");
+  $(".card-text").removeClass("text-light");
+  $("input").removeClass("bg-dark text-light");
+  localStorage.setItem("theme", "light");
+}
+
+function changeTheme() {
+  if (localStorage.getItem("theme") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+}
+
+$("#theme-btn").click(changeTheme);
+
+if (localStorage.getItem("theme") == "light") {
+  lightMode();
+} else {
+  darkMode();
+}
